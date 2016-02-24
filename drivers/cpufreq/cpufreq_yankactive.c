78d2fee00c26b7dbd991b2410a3fe8239fa1a659
spezi77 <spezi77@gmx.com>
2016-02-25 00:36:40 +0100

    Fix compilation: get_cpu_idle_time
    Change-Id: Ie5edd9227a0e33472cfc2d4c005f6a710e49ca05
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- drivers/cpufreq/cpufreq_yankactive.c
+++ drivers/cpufreq/cpufreq_yankactive.c
@@ -174,7 +174,7 @@ static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
 	return jiffies_to_usecs(idle_time);
 }
 
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu,
+static inline cputime64_t get_cpu_idle_time2(unsigned int cpu,
 					    cputime64_t *wall)
 {
 	u64 idle_time = get_cpu_idle_time_us(cpu, wall);
@@ -195,7 +195,7 @@ static void cpufreq_yankactive_timer_resched(
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time(smp_processor_id(),
+		get_cpu_idle_time2(smp_processor_id(),
 				     &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
@@ -234,7 +234,7 @@ static void cpufreq_yankactive_timer_start(int cpu, int time_override)
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp);
+		get_cpu_idle_time2(cpu, &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
 	spin_unlock_irqrestore(&pcpu->load_lock, flags);
@@ -378,7 +378,7 @@ static u64 update_load(int cpu)
 	unsigned int cur_load = 0;
 #endif
 
-	now_idle = get_cpu_idle_time(cpu, &now);
+	now_idle = get_cpu_idle_time2(cpu, &now);
 	delta_idle = (unsigned int)(now_idle - pcpu->time_in_idle);
 	delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp);
 
